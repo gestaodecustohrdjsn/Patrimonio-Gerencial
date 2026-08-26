5e9b7e793ff797165e16826ac36c033493ff1bb7
gestaodecustohrdjsn <gestaodecustohrdjsn@gmail.com>
2026-08-26 15:10:26 -0400

    Delete js/runtime-v12 directory

--- js/runtime-v12/config.js
+++
@@ -1,5 +0,0 @@
-// Configuração pública do Patrimônio+.
-// O navegador conhece SOMENTE a URL da nossa API.
-window.APP_CONFIG = {
-  API_BASE_URL: "https://patrimonio-api.patrimonio-gerencial.workers.dev"
-};
